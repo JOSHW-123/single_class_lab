657b497c153ac7e7d5d1ab2b5db99d2f63db007a
Josh <joshwilkinson33@gmail.com>
2021-06-21 20:00:14 +0100

    Test 2 complete

--- single_class_lab_start_code/student_class/src/student.py
+++ single_class_lab_start_code/student_class/src/student.py
@@ -2,4 +2,7 @@ class Student:
     def __init__(self, input_name, input_cohort):
         self.name = input_name
         self.cohort = input_cohort
-       
+
+    def student_has_cohort(input_cohort):
+        return input_cohort("name")
+        
